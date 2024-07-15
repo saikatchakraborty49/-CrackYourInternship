b3239564c505c7fae4c705a59ba5379c6a53d740
Saikat <saikatchakraborty416@gmail.com>
2024-07-15 12:07:01 +0530

    map to unordered map

--- Arrays/subArraySumDivisibleBy5.cpp
+++ Arrays/subArraySumDivisibleBy5.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
         int sum=0;
         int n=nums.size();
         mpp[0]=1;
